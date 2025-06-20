5036d1940ba684de04dc1c10d4b90a2ba3fb9517
Sub-esh <subsonicu571@gmail.com>
2025-06-20 10:31:00 +0545

    removed bug on main line 30

--- SimpleMapDisplay/simpleWidgets.py
+++ SimpleMapDisplay/simpleWidgets.py
@@ -4,9 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #title
-set.title("Streamlit App to Demonstrate Widget Functions")
+slt.title("Streamlit App to Demonstrate Widget Functions")
+
 #sidebar
-set.sidebar.header("Sidebar Options")
+slt.sidebar.header("Sidebar Options")
+
 #text/number input
-st.subheader("Text Input")
-st.write("You Entered:",st.text_input("Enter a text:"))
+slt.subheader("Text Input")
+slt.write("You Entered:",slt.text_input("Enter a text:"))
+slt.subheader("Number Input")
+slt.write("Number Input",slt.number_input("Number Input"))
+
+#slider
+slt.subheader("Slider")
+age = slt.slider("Select you age",0,100,25)
+slt.write("You are:", age, "Years old")
+
+#Button
+slt.subheader("Button")
+if slt.button("Click Me"):
+    slt.write("Button Clicked!")
+
+#Radio Button
+slt.subheader("Radio Button")
+YourSelection = slt.radio("Make a selection:",["Choice1","Choice2","Choice3"])
+slt.write("Your selection:",YourSelection)
