867e3c40744c169c33539c8dc2796211ad6290bc
Sub-esh <subsonicu571@gmail.com>
2025-06-20 23:37:57 +0545

    add the main parts of the program before streamlit portion

--- SimpleMapDisplay/GEEStreamlit/simpleClimateGEE.py
+++ SimpleMapDisplay/GEEStreamlit/simpleClimateGEE.py
@@ -1,6 +1,7 @@
 import streamlit as slt
 import ee
 import xarray as xr
+import matplotlib.pyplot as plt
 
 # authenticate and initialize gee
 ee.Authenticate()
@@ -14,3 +15,13 @@ data = ee.ImageCollection("ECMWF/ERA5_LAND/DAILY_AGGR").filterDate("2010","2025"
 data_ds = xr.open_dataset(data, engine = "ee")
 
 # convert dataset value from degree kelvin to degree celcius
+cel_data = data_ds - 273.15
+
+# calculate longterm average temperature and the temperature anomaly
+yearly_mean = temp_ds.resample(time = 'YE').mean('time')
+longterm_mean = yearly_mean.resample.mean('time')
+anomaly = yearly_mean - longterm_mean
+
+# visualize longterm mean temperature
+plt,ax = plt.subplots(figsize = (7,4))
+longterm_mean["temperature_2m"].plot( x = 'lon', y = 'lat', ax = ax)
