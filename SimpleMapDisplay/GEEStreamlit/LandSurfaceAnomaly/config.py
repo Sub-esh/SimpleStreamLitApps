a88134f53ccb8b536ff725d0f73e0cfb0b747d68
Sub-esh <subsonicu571@gmail.com>
2025-07-04 15:23:10 +0545

    finalized config.py file

--- SimpleMapDisplay/GEEStreamlit/LandSurfaceAnomaly/config.py
+++ SimpleMapDisplay/GEEStreamlit/LandSurfaceAnomaly/config.py
@@ -1,6 +1,6 @@
 # Configuration settings
 CONFIG = {
-    "NEPAL_DISTRICTS_ASSET": "F:\StreamlitApps\SimpleStreamLitApps\SimpleMapDisplay\GEEStreamlit\LandSurfaceAnomaly\",
+    "NEPAL_DISTRICTS_ASSET": "SimpleMapDisplay\GEEStreamlit\LandSurfaceAnomaly\assests\nepalAdminWGS84.shp",
     "DEFAULT_CENTER": [28.3949, 84.1240],  # Latitude, Longitude
     "DEFAULT_ZOOM": 7,
     "MAP_TYPES": ["ROADMAP", "HYBRID", "SATELLITE", "TERRAIN"],
